88d406783bd1c04e2d65299bf7f3b2ab50c0a355
taohungyang <proty.ece96@g2.nctu.edu.tw>
2017-09-13 11:14:35 -0400

    Suggestion for the fraction number processing
    Suggestion for the fraction number.
    Please review and see if you agree with it or if there is any bug

--- c7n/resources/account.py
+++ c7n/resources/account.py
@@ -494,7 +494,7 @@ class RequestLimitIncrease(BaseAction):
         'properties': {
             'type': {'enum': ['request-limit-increase']},
             'percent-increase': {'type': 'number', 'minimum': 1},
-            'resource-count': {'type': 'number', 'minimum': 1},
+            'amount-increase': {'type': 'number', 'minimum': 1},
             'subject': {'type': 'string'},
             'message': {'type': 'string'},
             'severity': {'type': 'string', 'enum': ['urgent', 'high', 'normal', 'low']}
@@ -528,35 +528,22 @@ class RequestLimitIncrease(BaseAction):
         region_map = {}
         limit_exceeded = resources[0].get('c7n:ServiceLimitsExceeded', [])
         percent_increase = self.data.get('percent-increase')
-        resource_count = self.data.get('resource-count')
+        amount_increase = self.data.get('amount-increase')
 
-        if percent_increase:
-            for s in limit_exceeded:
-                current_limit = int(s['limit'])
-                if current_limit <= 10:
-                    increase_by = float(self.data.get('percent-increase') / 10)
-                else:
-                    increase_by = current_limit * float(self.data.get('percent-increase')) / 100
-                increase_by = round(increase_by)
-                msg = '\nIncrease %s by %d in %s \n\t Current Limit: %s\n\t ' \
-                      'Current Usage: %s\n\t ' \
-                      'Set New Limit to: %d' % (
-                          s['check'], increase_by, s['region'], s['limit'], s['extant'],
-                          (current_limit + increase_by))
-                service_map.setdefault(s['service'], []).append(msg)
-                region_map.setdefault(s['service'], s['region'])
-        else:
-            for s in limit_exceeded:
-                current_limit = int(s['limit'])
-
-                increase_by = resource_count
-                msg = '\nIncrease %s by %d in %s \n\t Current Limit: %s\n\t ' \
-                      'Current Usage: %s\n\t ' \
-                      'Set New Limit to: %d' % (
-                          s['check'], increase_by, s['region'], s['limit'], s['extant'],
-                          (current_limit + increase_by))
-                service_map.setdefault(s['service'], []).append(msg)
-                region_map.setdefault(s['service'], s['region'])
+        for s in limit_exceeded:
+            current_limit = int(s['limit'])
+            if percent_increase:
+                increase_by = current_limit * float(percent_increase) / 100
+                increase_by = max(increase_by, 1)
+            else:
+                increase_by = amount_increase
+            increase_by = round(increase_by)
+            msg = '\nIncrease %s by %d in %s \n\t Current Limit: %s\n\t Current Usage: %s\n\t ' \
+                  'Set New Limit to: %d' % (
+                      s['check'], increase_by, s['region'], s['limit'], s['extant'],
+                      (current_limit + increase_by))
+            service_map.setdefault(s['service'], []).append(msg)
+            region_map.setdefault(s['service'], s['region'])
 
         for service in service_map:
             subject = self.data.get('subject', self.default_subject).format(
